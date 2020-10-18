bfe5d59631563414acb39de1c58527a026fb0219
Prophet <abd97mob@gmail.com>
2020-10-18 18:11:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.ageinminutes
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnDatePicker.setOnClickListener {
+            Toast.makeText(this,"Button Works", Toast.LENGTH_LONG).show() }
+    }
+
+}
+
+fun clickDatePicker(view: View) {
+
+    DatePickerDialog(this, DatePickerDialog, DatePickerDialog.OnDateSetListener())
+}
