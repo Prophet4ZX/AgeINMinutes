00fcf457ebc4ea2c75f51a0378a5d921d6bb3db4
Prophet <abd97mob@gmail.com>
2020-10-20 12:07:56 +0300

    .

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -6,23 +6,47 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnDatePicker.setOnClickListener {
-            Toast.makeText(this,"Button Works", Toast.LENGTH_LONG).show() }
+        btnDatePicker.setOnClickListener {view -> clickDatePicker(view)
+            }
+}
+
+    fun clickDatePicker(view: View) {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+       DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+           Toast.makeText(this, "The chosen date is $selectedDayOfMonth/${selectedMonth+1}/$selectedYear",Toast.LENGTH_LONG).show()
+       val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+
+           tvSelectedDate.text = selectedDate
+
+           val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+
+           val theDate = sdf.parse(selectedDate)
+
+           val theDateInMinutes = theDate!!.time / 60000
+
+           val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+
+           val currentDateInMinutes = currentDate!!.time / 60000
+
+           val differenceInMinutes = currentDateInMinutes - theDateInMinutes
+
+           tvSelectedDateInMinutes.text = differenceInMinutes.toString()
+       }
+           ,year, month, day).show()
+
+
+
     }
-
 }
-
-/*fun clickDatePicker(view: View) {
-
-   // DatePickerDialog(this, DatePickerDialog, DatePickerDialog.OnDateSetListener())
-    //2
-
-}
-
- */
